79c9a3b0d50206a4226d5df8a1697aa92cbbb689
Priyanshu Priya <priyanshupriyacodes@gamil.com>
2025-09-10 15:28:40 +0530

    back to the prev function code

--- netlify/functions/get-news.js
+++ netlify/functions/get-news.js
@@ -2,24 +2,11 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   const apiKey = process.env.NEWS_API_KEY;
-  
-  const query = event.queryStringParameters.q || ''; 
-  
-  const apiURL = `https://newsapi.org/v2/top-headlines?country=in&q=${query}&apiKey=${apiKey}`;
+  const query = event.queryStringParameters.q || 'India'; // Get the query from the URL or default to 'India'
+  const apiURL = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey}`;
 
   try {
     const response = await fetch(apiURL);
-
-    // Add this check to handle non-successful HTTP status codes
-    if (!response.ok) {
-      // Get the error message from the API response
-      const errorData = await response.json(); 
-      return {
-        statusCode: response.status,
-        body: JSON.stringify({ error: `News API error: ${errorData.message}` })
-      };
-    }
-
     const data = await response.json();
     return {
       statusCode: 200,
